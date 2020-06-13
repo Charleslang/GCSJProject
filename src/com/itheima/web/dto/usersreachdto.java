fcfe33c58039d9b528cd0715373ca7286185db64
CK7777 <1535389112@qq.com>
2020-06-13 09:41:34 +0800

    ck

--- src/com/itheima/web/dto/usersreachdto.java
+++ src/com/itheima/web/dto/usersreachdto.java
@@ -6,4 +6,31 @@ package com.itheima.web.dto;
  * @Version 1.0
  */
 public class usersreachdto {
+    private String start;
+    private String end;
+    private String uname;
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
 }
+
